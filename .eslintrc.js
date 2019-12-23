64c89de4765f149d9e85e4f0f644cfc618b64df2
farkmarnum <mark.farnum@gmail.com>
2019-12-22 22:09:34 -0500

    init

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  parser: 'babel-eslint',
+
+  extends: [
+    'airbnb',
+    'prettier',
+  ],
+
+  plugins: ['prettier'],
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    // Forbid the use of extraneous packages
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+    'import/no-extraneous-dependencies': ['error', { packageDir: '.' }],
+
+    // Recommend not to leave any console.log in your code
+    // Use console.error, console.warn and console.info instead
+    // https://eslint.org/docs/rules/no-console
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info'],
+      },
+    ],
+
+    // Ignore Mongo's `_id`
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+
+    // "Prefer default export" can be annoying when setting up modules that will later scale to more than one export
+    'import/prefer-default-export': 'warn',
+
+    // Prefer destructuring from arrays and objects
+    // http://eslint.org/docs/rules/prefer-destructuring
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+
+    // ESLint plugin for prettier formatting
+    // https://github.com/prettier/eslint-plugin-prettier
+    'prettier/prettier': 'error',
+  },
+
+  settings: {
+    // Allow absolute paths in imports, e.g. import Button from 'components/Button'
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+}
